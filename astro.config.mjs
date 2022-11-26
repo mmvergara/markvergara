a4a6a3b06638d6a88452644ecf8357ec85a74107
mmvergara <mark.jesusmanabat@gmail.com>
2022-11-26 08:22:48 +0800

    optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+import compress from "astro-compress";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  site: "https://markvergara.vercel.app/",
+  base: "/",
+  integrations: [
+    tailwind(),
+    react(),
+    sitemap(),
+    compress({ css: true, html: true, js: true, img: true, svg: true }),
+  ],
 });
