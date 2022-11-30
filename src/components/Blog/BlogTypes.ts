3bd97ca3d3c832e6429613ae5af366ec82fcc102
mmvergara <mark.jesusmanabat@gmail.com>
2022-11-30 12:48:27 +0800

    added blogs

---
+++ src/components/Blog/BlogTypes.ts
@@ -0,0 +1,12 @@
+export interface BlogListTypes {
+  id: number;
+  title: string;
+  description: string;
+  url: string;
+  comments_count: number;
+  public_reactions_count: number;
+  positive_reactions_count: number;
+  last_comment_at: string;
+  tags: string;
+  published_at:string;
+}
---
+++ src/components/Blog/Markdown.jsx
@@ -0,0 +1,36 @@
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { nightOwl } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import styles from "./markdown.module.css";
+
+const Markdown = ({ markdown }) => {
+  return (
+    <span className={styles.nice}>
+      <ReactMarkdown
+        children={markdown}
+        remarkPlugins={[remarkGfm]}
+        components={{
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || "");
+            return !inline && match ? (
+              <SyntaxHighlighter
+                children={String(children).replace(/\n$/, "")}
+                style={nightOwl}
+                language='javascript'
+                PreTag='div'
+                {...props}
+              />
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            );
+          },
+        }}
+      />
+    </span>
+  );
+};
+
+export default Markdown;
