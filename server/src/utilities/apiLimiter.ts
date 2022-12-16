df81978ac8375fe1940739b687b440fa799abf9b
mmvergara <mark.jesusmanabat@gmail.com>
2022-12-17 04:36:40 +0800

    removed server

--- server/src/utilities/apiLimiter.ts
+++
@@ -1,8 +0,0 @@
-import expressRateLimiter from "express-rate-limit";
-
-export const limiter = expressRateLimiter({
-  windowMs: 60 * 60 * 1000,
-  max: 20,
-  standardHeaders: true,
-  legacyHeaders: false,
-});
