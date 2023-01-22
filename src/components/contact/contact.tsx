d955c53bb980ec2774d1fc8aa96cc9329d60d69f
mmvergara <mark.jesusmanabat@gmail.com>
2023-01-22 13:45:53 +0800

    added Contact-Box

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -36,79 +36,9 @@ const ContactPage = () => {
   };
   return (
     <>
-      <h1 className='font-Poppins text-center text-white text-3xl sm:text-5xl md:text-7xl' style={{ color: "#eab308" }}>
-        Contact Me
-      </h1>
 
-      <section className='text-white text-sm font-medium bg-skillCardBg p-4 flex flex-col w-[350px] rounded-lg border-b-4 border-cardBg mx-auto mt-8'>
-        <p>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            className='inline mx-1'
-            width='24'
-            height='24'
-            viewBox='0 0 24 24'
-            stroke='currentColor'
-            strokeWidth='2'
-            fill='none'
-          >
-            <path stroke='none' d='M0 0h24v24H0z' fill='none'></path>
-            <rect x='3' y='5' width='18' height='14' rx='2'></rect>
-            <polyline points='3 7 12 13 21 7'></polyline>
-          </svg>
-          :{" "}
-          <a href='mailto:mark.jesusmanabat@gmail.com' aria-label='Email Address' className='text-typescript'>
-            Mark.jesusmanabat@gmail.com
-          </a>
-        </p>
-        <p>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            className='inline mx-1'
-            width='24'
-            height='24'
-            viewBox='0 0 24 24'
-            strokeWidth='2'
-            stroke='currentColor'
-            fill='none'
-          >
-            <path stroke='none' d='M0 0h24v24H0z' fill='none'></path>
-            <path d='M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5'></path>
-          </svg>
-          :{" "}
-          <a href='https://github.com/mmvergara' aria-label='Github Profile' className='text-typescript'>
-            @mmvergara
-          </a>
-        </p>
-        <p>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            className='inline mx-1'
-            width='24'
-            height='24'
-            viewBox='0 0 24 24'
-            strokeWidth='2'
-            stroke='currentColor'
-            strokeLinecap='round'
-            fill='none'
-          >
-            <path stroke='none' d='M0 0h24v24H0z' fill='none'></path>
-            <rect x='4' y='4' width='16' height='16' rx='2'></rect>
-            <line x1='8' y1='11' x2='8' y2='16'></line>
-            <line x1='8' y1='8' x2='8' y2='8.01'></line>
-            <line x1='12' y1='16' x2='12' y2='11'></line>
-            <path d='M16 16v-3a2 2 0 0 0 -4 0'></path>
-          </svg>
-          :{" "}
-          <a
-            href='https://www.linkedin.com/in/mark-vergara-8436b0243/'
-            aria-label='LinkedIn Profile'
-            className='text-typescript'
-          >
-            Mark Vergara
-          </a>
-        </p>
-      </section>
+
+      
 
       <form
         onSubmit={submitMessage}
