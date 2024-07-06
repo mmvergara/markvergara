2fc0dadb71adb3cbd032e60762a98bdb5b0827d2
mmvergara <mark.jesusmanabat@gmail.com>
2024-07-06 11:42:56 +0800

    refactor: Update TechnologiesArr to include all available technologies

---
+++ src/types.ts
@@ -0,0 +1,44 @@
+export type Technologies =
+  | "React"
+  | "NextJS"
+  | "Typescript"
+  | "Tailwind"
+  | "Dart"
+  | "Flutter"
+  | "Golang"
+  | "Python"
+  | "NodeJS"
+  | "Firebase"
+  | "MongoDB"
+  | "PostgreSQL"
+  | "Supabase"
+  | "GraphQL"
+  | "Vue";
+
+export type Project = {
+  title: string;
+  imageUrl: string;
+  description: string;
+
+  technologies: Technologies[];
+  liveUrl?: string;
+  githubUrl: string;
+  date: Date;
+};
+
+const TechnologiesArr: Technologies[] = [
+  "React",
+  "NextJS",
+  "Typescript",
+  "Tailwind",
+  "Dart",
+  "Flutter",
+  "Golang",
+  "Python",
+  "NodeJS",
+  "Firebase",
+  "MongoDB",
+  "PostgreSQL",
+  "Supabase",
+  "Vue",
+];
