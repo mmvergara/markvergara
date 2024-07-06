d2523378eddb4e6090065150aa2a52a895d17609
mmvergara <mark.jesusmanabat@gmail.com>
2024-07-06 11:34:17 +0800

    refactor: Remove unnecessary xs screen size from Tailwind CSS config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,7 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      screens: {
-        xs: "520px",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
