144080368400f0f756ce2db10cc52ade5d4de764
mmvergara <mark.jesusmanabat@gmail.com>
2024-11-28 17:03:28 +0800

    add analytics

--- src/page/layout.tsx
+++ src/page/layout.tsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import { useTheme } from "../context/ThemeContext";
 import clsx from "clsx";
+import { Analytics } from '@vercel/analytics/react';
 
 const Layout = () => {
   const { isBrutalism } = useTheme();
@@ -16,6 +17,7 @@ const Layout = () => {
       <main className="mx-auto w-full max-w-[800px]">
         <Outlet />
       </main>
+      <Analytics />
     </div>
   );
 };
